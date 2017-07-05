c94d14f5bfd87f64bbe57fb7e86d893dbb0cdcfa
Div <AliveDiv@gmail.com>
2017-07-05 11:19:48 +0300

    MVVM init

---
+++ GkMic/GkMic/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using GkMic.ViewModel;
+
+namespace GkMic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
