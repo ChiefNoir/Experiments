2e7a3d608d73592130a7094f3651324f002e33ba
Div <AliveDiv@gmail.com>
2017-07-05 12:43:22 +0300

    + Contracts for data service

---
+++ GkMic/Contracts/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using DataModel;
+using System;
+
+namespace Contracts
+{
+    public interface IDataService
+    {
+        void Save(Action<Passport, Exception> callback);
+
+        void Save(Action<BirthCertificate, Exception> callback);
+    }
+}
